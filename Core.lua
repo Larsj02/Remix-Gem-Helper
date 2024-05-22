af0561c426e6ae046a42e62acfb9d205e10d0356
MarkW <numedain@gmail.com>
2024-05-22 13:38:00 +0200

    prevent the frame from loading and erroring outside of timerunning

--- Core.lua
+++ Core.lua
@@ -491,6 +491,7 @@ eventFrame:SetScript("OnEvent", function(_, event)
         end)
     end
     if event ~= "PLAYER_ENTERING_WORLD" then return end
+    if 1 ~= PlayerGetTimerunningSeasonID() then return end
 
     for itemID in pairs(const.GEM_SOCKET_TYPE) do
         cache:CacheItemInfo(itemID)
