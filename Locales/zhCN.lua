56715f74b9103cc1f7c31364d795762c65a75d52
Moom <fallin1992@gmail.com>
2024-05-29 00:51:24 +0800

    Updated localisation strings to based on 1.5.0d

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -3,24 +3,30 @@ local Private = select(2, ...)
 Private.Locales = Private.Locales or {}
 
 Private.Locales["zhCN"] = {
-    ["Remix Gem Helper"] = "幻彩再造宝石助手",
     --isEditing = true,
+
+    -- addon title
+    ["Remix Gem Helper"] = "幻彩再造宝石助手",
+
+    -- gem types
     ["All"] = "全部",
     ["Meta"] = "多彩宝石",
     ["Cogwheel"] = "齿轮宝石",
     ["Tinker"] = "匠械宝石",
     ["Prismatic"] = "棱彩宝石",
 
-    -- CloakTooltip.lua
+    -- ItemTooltips.lua
     ["Rank"] = "等级",
+    ["Upgradeable"] = "可升级",
+    ["Not Upgradeable"] = "不可升级",
 
     -- Core.lua
     ["Socketed"] = "已镶嵌",
     ["In Bag"] = "在背包",
     ["In Bag Item!"] = "镶嵌于背包装备!",
     ["Uncollected"] = "未获得",
-    ["Scrappable Items"] = "灰色装备",
-    ["NOTHING TO SCRAP"] = "没有垃圾",
+    ["Scrappable Items"] = "可拆解物品",
+    ["NOTHING TO SCRAP"] = "*无可拆解物品*",
     ["Resocket Gems"] = "重新镶嵌宝石",
     ["Toggle the %s UI"] = "切换 %s 界面", -- %s is the Addon name and needs to be included!
     ["Search Gems"] = "搜索宝石",
@@ -29,7 +35,7 @@ Private.Locales["zhCN"] = {
     ["Primordial"] = "始源之石",
     ["Show Primordial Gems in the List."] = "在列表中显示始源之石。",
     ["Open, Use and Combine"] = "可打开，使用或组合",
-    ["NOTHING TO USE"] = "无",
+    ["NOTHING TO USE"] = "*无*",
     ["HelpText"] =
         "|A:newplayertutorial-icon-mouse-leftbutton:16:16|a Click a Gem in this list to Socket or Unsocket.\n" ..
         "'In Bag Item' or 'Socketed' indicates that you unsocket it.\n" ..
